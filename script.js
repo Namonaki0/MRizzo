1ab95bda375628a4478e3945c91fde4c00179795
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-11-29 23:53:35 +0000

    page content and styling

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const burgerMenu = document.querySelector(".burger-menu");
 const nav = document.querySelector("nav");
 const navUl = document.querySelector("ul");
+const navLis = document.querySelectorAll("nav > ul > li");
 const menuBarOne = document.querySelector(".bar-one");
 const menuBarTwo = document.querySelector(".bar-two");
 const menuBarThree = document.querySelector(".bar-three");
+const routerOutlet = document.querySelector("#router-outlet");
 
 burgerMenu.addEventListener("click", () => {
   navUl.classList.toggle("open-menu");
   nav.classList.toggle("open-menu");
+  routerOutlet.classList.toggle("open-menu");
   menuBarOne.classList.toggle("effect");
   menuBarTwo.classList.toggle("effect");
   menuBarThree.classList.toggle("effect");
+});
+
+navLis.forEach((navLi) => {
+  navLi.addEventListener("click", () => {
+    nav.classList.remove("open-menu");
+    navUl.classList.remove("open-menu");
+    menuBarOne.classList.remove("effect");
+    menuBarTwo.classList.remove("effect");
+    menuBarThree.classList.remove("effect");
+    routerOutlet.classList.remove("open-menu");
+  });
 });
 
 import RouterHandler from "./router.js";
--- sections/home.js
+++ sections/home.js
@@ -1,23 +1,23 @@
 export default `
 
-    <h1>Home</h1>
 
-    <div class="bio">
+    <div class="bio">   
 
        
 
-        <h2>M Rizzo</h2>
+  
 
-         <img src="https://www.metal-archives.com/images/4/8/3/6/48367_photo.jpg?1901" />
+        <div class="img-wrapper">
+         <img src="https://metaladdicts.com/wp-content/uploads/2016/06/Marc-Rizzo.jpg" />
+        </div> 
+
+        <div class="bio-description-wrapper">
+
+              <h2>M Rizzo</h2>
 
         <p>Marc Rizzo is an American guitarist best known for his presence in metal band Soulfly, formerly of ill Nino and on the rise solo projects Acoustic Vendetta and Revenge Beast.</p>
 
-​        <p>Recently Rizzo has stepped out with his own solo projects that reveal a phenomenal command of   the guitar. Colossal Myopia, The Ultimate Devotion, Legionnaire & Rotation reveals Rizzo to be an even more Master Musician, displaying deep musical roots in Metal, Classical, and Flamenco.</p>
-
-​
-
-        <p>Marc Rizzo is a breath of fresh air to the Instrumental Rock Genre.</p>
-
+        </div>
     </div>
 
     <div class="upcoming-shows">
