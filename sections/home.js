576ead3737eb82c48299743e1bbc36c93588c571
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-12-01 23:41:38 +0000

    merch page

--- sections/home.js
+++ sections/home.js
@@ -3,10 +3,6 @@ export default `
 
     <div class="bio">   
 
-       
-
-  
-
         <div class="img-wrapper">
          <img src="https://metaladdicts.com/wp-content/uploads/2016/06/Marc-Rizzo.jpg" />
         </div> 
@@ -19,6 +15,20 @@ export default `
 
         </div>
     </div>
+
+    <section class="track-previews">
+
+    <h2>previews</h2>
+
+    <iframe src="https://open.spotify.com/embed/album/5z7NkjFyLKFA75yID4nkbH?utm_source=generator" width="95%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>    
+
+    <iframe src="https://open.spotify.com/embed/album/0NhTzQ1uuR7YUOTsqUP7kR?utm_source=generator" width="95%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+
+    <iframe src="https://open.spotify.com/embed/album/0ceQJvREwK9rAh3bxytNHw?utm_source=generator" width="95%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+
+    <iframe src="https://open.spotify.com/embed/album/6Iu3IMF17LWMEDGG5Kr8Ax?utm_source=generator" width="95%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+
+    </section>
 
     <div class="upcoming-shows">
         <h2>upcoming shows</h2>
--- sections/merch.js
+++ sections/merch.js
@@ -1,5 +1,20 @@
 export default `
 
     <h1>Merch</h1>
+
+    <section class="merch-display">
+        <img src="https://www.storefrontier.com/sites/default/files/product_mockups/66357/rizzo_7_170032_1083_325093_470_black170746_nobg.png?1638400254" loading="lazy"/>
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_1_155452_1083_324971_470_black155810_nobg.png?itok=ljd-UQGE" loading="lazy">
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_2_160250_1083_324990_470_black160826_nobg.png?itok=buJkTnTz" loading="lazy">
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_3_161224_1083_325007_470_black161529_nobg.png?itok=9LLiVPvo" loading="lazy">
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_5_164140_1083_325064_470_black164615_nobg.png?itok=juaAOPEX" loading="lazy">
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_8_171056_1083_325113_470_black171801_nobg.png?itok=AAhl2EjB" loading="lazy">
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_9_172200_1083_325128_470_black172607_nobg.png?itok=kkX_x-u_" loading="lazy">
+        <img src="https://www.storefrontier.com/sites/default/files/styles/product_listing_large/public/product_mockups/66357/rizzo_10_173139_1083_325150_470_black173515_nobg.png?itok=m2pDW7zm"loading="lazy"/>
+    </section>
+
+    <div class="frontier-shop">
+        <a href="https://www.storefrontier.com/marcrizzo" target="_blank">more merch</a>
+    </div>
     
     `;
