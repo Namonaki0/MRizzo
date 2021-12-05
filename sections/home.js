5a9d256a9a198dc592dc65448f79fa7718eed48b
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-12-05 23:21:36 +0000

    layout changes

--- sections/home.js
+++ sections/home.js
@@ -33,9 +33,9 @@ export default `
     <div class="upcoming-shows">
         <h2>upcoming shows</h2>
         <div class="img-wrapper">
-        <img src="https://static.wixstatic.com/media/6a8a07_89075a64c3f24a78bc6b3a40fc3c7b71~mv2.jpeg/v1/fill/w_900,h_900,al_c,q_85,usm_0.66_1.00_0.01/Hail%20the%20Horns%20and%20Whiskey%20Dick_JPEG.webp">
+        <img src="images/Hail_The_Horns_And_Whiskey_Dick.jpeg" alt="Hail the Horns and Whiskey Dick banner">
       
-        <img src="https://static.wixstatic.com/media/6a8a07_68cae8f02c5142a28291bec07a395e5f~mv2.jpg/v1/fill/w_731,h_900,al_c,q_85,usm_0.66_1.00_0.01/IMG_3921.webp">
+        <img src="images/Winter_Wasteland.jpeg" alt="Winter Wasteland banner">
         </div>
     </div>
     
